2ea8921fc64cb20f318d7562439a54c778ab1542
Supapond <supapondmaliwab@gmail.com>
2025-10-01 21:26:01 +0700

    fead:ex-1 employeeSalaries5000

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,22 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
+
 
 function forEach(array, operation) {
   // Start coding here
+  const newEmployeeSalaries = [];
+  for(let i=0; i < array.length; i++){
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+  return newEmployeeSalaries;
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries,addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +26,9 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+- addSalary5000
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+- forEach
 ====================================
 */
