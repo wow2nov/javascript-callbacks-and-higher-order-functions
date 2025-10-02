031a10beb94127a34dd84b72830ec731a6912402
Supapond <supapondmaliwab@gmail.com>
2025-10-02 15:56:28 +0700

    fead:ex-2 checkscore70

--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,24 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  return score > 70;
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for(let i = 0; i < array.lenght; i++){
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+  return count >= 5;
 }
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
@@ -24,8 +32,10 @@ console.log(scoreRoom3Result); //false
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+- checkGradeOver70
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+- atLeastFive
 
 ====================================
 */
